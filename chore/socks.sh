593abf50e47e5e3944b2080f10eb40449ef3d730
tenax66 <hiyoshir+tenax66@gmail.com>
2024-04-10 00:30:54 +0900

    add a script for tor

---
+++ chore/socks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+trap 'sudo networksetup -setsocksfirewallproxystate Wi-fi off' 2
+
+sudo networksetup -setsocksfirewallproxy Wi-fi localhost 9050
+sudo networksetup -setsocksfirewallproxystate Wi-fi on
+
+pproxy -l http://:8181 -r socks5://127.0.0.1:9050 -vv & tor
